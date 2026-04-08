d2d335f6202d5f2d857d0cd29429b3b9a2aa8ee2
Nikola Tucković <nikola.tuckovic@styria.dev>
2026-04-08 09:34:08 +0200

    raise packages

--- static/script/main.js
+++ static/script/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'https://unpkg.com/petite-vue?module'
+import { createApp } from 'https://unpkg.com/petite-vue@0.4.1/dist/petite-vue.es.js'
 
 import Skills from './components/skills.js'
 import Experience from './components/experience.js'
